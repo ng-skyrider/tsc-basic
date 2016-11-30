bbc770bf2b55645ba8e8dba1cd79b18fa2a8db80
FredPizarro <freddy_ps_3@hotmail.com>
2016-11-30 11:10:18 -0500

    217.0_Creating.a.TypeScript-project

---
+++ src/app.js
@@ -0,0 +1,5 @@
+// comment   :::::::   tsc [file-app].ts         -->  "tsc" (typescript) - create JS
+/*
+*   tsc -w  [file-app].ts  --> "-w"  Run the compiler in watch mode
+*/
+var holaMundo = "hello Mundo !!...";
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+// comment   :::::::   tsc [file-app].ts         -->  "tsc" (typescript) - create JS
+
+/*
+*   tsc -w  [file-app].ts  --> "-w"  Run the compiler in watch mode
+*/
+
+let holaMundo: string = "hello Mundo !!..."
