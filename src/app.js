6dfeca225dd757fd91e9ea884d0a16a6de9d2994
FredPizarro <freddy_ps_3@hotmail.com>
2016-12-13 15:23:38 -0500

    218.0_Optional.and.Default.Parameters_Template.strings

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 // comment   :::::::   tsc [file-app].ts         -->  "tsc" (typescript) - create JS
 /*
 *   tsc -w  [file-app].ts  --> "-w"  Run the compiler in watch mode
+*
+*  doc (parameters):  https://www.typescriptlang.org/docs/handbook/functions.html#optional-and-default-parameters
 */
 var holaMundo = "hello Mundo !!...";
+//    template
+var content = document.getElementById('content');
+var template = {
+    id: 156,
+    name: "skyrider",
+    completed: true
+};
+var backticks = "template " + template.id; /* generar (``): alt Gr + key */
+// method (parametros)
+function method(parameter1, parameter2) { }
+method("Texto simple in string", 10);
+content.innerHTML = "\n<div id='" + template.id + "'>\n    <i class=\" " + (template.completed ? " " : "hidden") + " \"></i>\n    <span> " + template.name + " </span>\n</div>\n";
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,30 @@
 
 /*
 *   tsc -w  [file-app].ts  --> "-w"  Run the compiler in watch mode
+*
+*  doc (parameters):  https://www.typescriptlang.org/docs/handbook/functions.html#optional-and-default-parameters
 */
 
 let holaMundo: string = "hello Mundo !!..."
+
+
+//    template
+var content = document.getElementById( 'content' );
+var template= { 
+        id: 156,
+        name: "skyrider",
+        completed: true
+}
+let backticks: string = `template ${template.id}`; /* generar (``): alt Gr + key */
+
+// method (parametros)
+function method( parameter1:string, parameter2){  }
+method("Texto simple in string", 10);
+
+content.innerHTML = `
+<div id='${template.id}'>
+    <i class=" ${ template.completed ? " ": "hidden" } "></i>
+    <span> ${template.name} </span>
+</div>
+`;
+
