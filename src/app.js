ab937de3559b9643899d246bb94e599847920e5e
FredPizarro <freddy_ps_3@hotmail.com>
2016-12-13 19:20:28 -0500

    219.0_var_let.AND.const_(Declarations)

--- src/app.js
+++ src/app.js
@@ -1,19 +1,82 @@
-// comment   :::::::   tsc [file-app].ts         -->  "tsc" (typescript) - create JS
 /*
-*   tsc -w  [file-app].ts  --> "-w"  Run the compiler in watch mode
-*
-*  doc (parameters):  https://www.typescriptlang.org/docs/handbook/functions.html#optional-and-default-parameters
+* Variable Declarations
 */
-var holaMundo = "hello Mundo !!...";
-//    template
-var content = document.getElementById('content');
-var template = {
-    id: 156,
-    name: "skyrider",
-    completed: true
+// " var "  declarations  (is scoped to the nearest function block )
+var diez = 10;
+var sumas = 12 + diez;
+var string = "var [name] = value";
+var ten = a();
+function a() {
+    return diez;
+}
+//  var-scoping or function-scoping (especificando-)
+function f(value) {
+    if (value) {
+        sumas = 15;
+    }
+    return sumas;
+}
+// f(true) --> 15  (return)
+// f(false) --> undefined (return)
+// Variable capturing quirks
+for (var i = 0; i < diez; i++) {
+    setTimeout(function () { console.log(i); }, 100 * i);
+}
+// 10
+// 10
+// 10
+// 10
+// 10
+// 10
+// 10
+// 10
+// 10
+// 10
+// " let " declarations (  is scoped to the nearest enclosing block (both are global if outside any block), which can be smaller than a function block. )
+//let var1 [= valor1] [, var2 [= valor2]] [, ..., varN [= valorN]];
+var hello = "Hello! ";
+var numero = 10; // Function declarations
+var texto = "Text"; // Function declarations
+//Block-scoping
+function f(value) {
+    if (value) {
+        var valor = 10 + diez; // `valor` block-scoped to `if` block!
+        console.log(valor); // 20 (result)    
+    }
+    console.log(valor); // ReferenceError: `valor` is undefined
+}
+// Re-declarations and Shadowing
+var valor2 = 20;
+var valor2 = 30; // ERROR: can't re-declare 'x' in the same scope
+var valor3 = 40;
+var valor3 = 40; // ERROR: can't have both declarations of 'x'
+function other(condicion, x) {
+    if (condicion) {
+        var x_1 = 20;
+        return x_1;
+    }
+    return x;
+}
+// other(false, 10);   --> 10
+// other(true, 10);   --> 20
+//Block-scoped variable capturing
+function f() {
+    var environment;
+    if (true) {
+        var texto_1 = "captured";
+        environment = function () {
+            return texto_1;
+        };
+    }
+    return environment;
+}
+// " const " declarations (constant cannot change through re-assignment, and it can't be redeclared. )
+// const name1 = value1 [, name2 = value2 [, ... [, nameN = valueN]]];
+var key = 'FZRD124 ';
+var person = {
+    name: 'SkyRider',
+    year: 24
 };
-var backticks = "template " + template.id; /* generar (``): alt Gr + key */
-// method (parametros)
-function method(parameter1, parameter2) { }
-method("Texto simple in string", 10);
-content.innerHTML = "\n<div id='" + template.id + "'>\n    <i class=\" " + (template.completed ? " " : "hidden") + " \"></i>\n    <span> " + template.name + " </span>\n</div>\n";
+//    person.name = Fred Pizarro
+// If you do need to freeze everything, we have something called Object.freeze. It’s actually not part of ES6
+var ahda = Object.freeze(person);
--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,109 @@
-// comment   :::::::   tsc [file-app].ts         -->  "tsc" (typescript) - create JS
-
 /*
-*   tsc -w  [file-app].ts  --> "-w"  Run the compiler in watch mode
-*
-*  doc (parameters):  https://www.typescriptlang.org/docs/handbook/functions.html#optional-and-default-parameters
+* Variable Declarations
 */
 
-let holaMundo: string = "hello Mundo !!..."
-
-
-//    template
-var content = document.getElementById( 'content' );
-var template= { 
-        id: 156,
-        name: "skyrider",
-        completed: true
+// " var "  declarations  (is scoped to the nearest function block )
+var diez = 10; 
+var sumas = 12 + diez;
+var string = "var [name] = value";
+var ten = a();
+function a(){
+    return diez;
 }
-let backticks: string = `template ${template.id}`; /* generar (``): alt Gr + key */
 
-// method (parametros)
-function method( parameter1:string, parameter2){  }
-method("Texto simple in string", 10);
+    //  var-scoping or function-scoping (especificando-)
+    
+    function f(value: boolean){
+        if(value){
+            sumas = 15;
+        }
+        return sumas;
+    }
+    // f(true) --> 15  (return)
+    // f(false) --> undefined (return)
 
-content.innerHTML = `
-<div id='${template.id}'>
-    <i class=" ${ template.completed ? " ": "hidden" } "></i>
-    <span> ${template.name} </span>
-</div>
-`;
+    // Variable capturing quirks
+    for( var i=0; i<diez; i++ ){
+        setTimeout( function(){ console.log(i); }, 100*i );
+    }
+    // 10
+    // 10
+    // 10
+    // 10
+    // 10
+    // 10
+    // 10
+    // 10
+    // 10
+    // 10
 
+
+
+
+// " let " declarations (  is scoped to the nearest enclosing block (both are global if outside any block), which can be smaller than a function block. )
+//let var1 [= valor1] [, var2 [= valor2]] [, ..., varN [= valorN]];
+
+let hello = "Hello! ";
+let numero: number = 10; // Function declarations
+let texto: string = "Text"; // Function declarations
+
+
+    //Block-scoping
+    function f(value: boolean){
+        
+        if(value){
+            let valor = 10+ diez; // `valor` block-scoped to `if` block!
+            console.log(valor); // 20 (result)    
+        }
+        
+        console.log(valor); // ReferenceError: `valor` is undefined
+    }
+    
+    // Re-declarations and Shadowing
+    let valor2 = 20;
+    let valor2 = 30; // ERROR: can't re-declare 'x' in the same scope
+    
+    let valor3 = 40;
+    var valor3 = 40; // ERROR: can't have both declarations of 'x'
+
+    function other(condicion, x){
+        
+        if(condicion){
+            let x = 20;
+            return x;
+        }
+        return x;
+    }
+    // other(false, 10);   --> 10
+    // other(true, 10);   --> 20
+
+    //Block-scoped variable capturing
+    function f(){
+        let environment;
+        
+        if(true){
+            let texto = "captured";
+            environment = function(){
+                return texto;
+            }
+        }
+        
+        return environment;
+    }
+    
+
+
+
+// " const " declarations (constant cannot change through re-assignment, and it can't be redeclared. )
+// const name1 = value1 [, name2 = value2 [, ... [, nameN = valueN]]];
+
+const key = 'FZRD124 ';
+
+    const person = {
+        name: 'SkyRider',
+        year: 24
+    }
+
+//    person.name = Fred Pizarro
+    // If you do need to freeze everything, we have something called Object.freeze. It’s actually not part of ES6
+    const ahda = Object.freeze(person);
